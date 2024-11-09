0ed5f0f3e54d0c4e8b9ddd0c9e4cf6d24c656518
Nurtilek Abduraimov <abduraimovnurtilek4@gmail.com>
2024-11-09 15:57:05 +0600

    pull

--- Dezis/Extensions/keyboardExtension.swift
+++ Dezis/Extensions/keyboardExtension.swift
@@ -11,15 +11,15 @@ extension UIViewController {
     
     func dismissKeyboardGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-               tapGesture.cancelsTouchesInView = false
-               view.addGestureRecognizer(tapGesture)
-               
-               let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-               swipeGesture.direction = .down
-               view.addGestureRecognizer(swipeGesture)
-           }
-
-           @objc private func dismissKeyboard() {
-               view.endEditing(true)
-           }
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        swipeGesture.direction = .down
+        view.addGestureRecognizer(swipeGesture)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
