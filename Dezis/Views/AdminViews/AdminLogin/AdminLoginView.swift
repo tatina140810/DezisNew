067d060c9313e964413174912f3480a691247a4c
telegeyN <ntelegey@gmail.com>
2024-11-19 19:30:25 +0600

    keyboard changes

--- Dezis/Views/AdminViews/AdminLogin/AdminLoginView.swift
+++ Dezis/Views/AdminViews/AdminLogin/AdminLoginView.swift
@@ -170,7 +170,42 @@ class AdminLoginView: UIViewController {
         setupNavigation()
         presenter = AdminLoginPresenter(view: self)
         dismissKeyboardGesture()
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+
+    deinit {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        let keyboardHeight = keyboardFrame.height
+        let bottomOffset = continueButton.frame.maxY - (view.frame.height - keyboardHeight)
+        
+        if bottomOffset > 0 {
+            UIView.animate(withDuration: animationDuration) {
+                self.view.frame.origin.y = -bottomOffset - 10
+            }
+        }
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.view.frame.origin.y = 0
+        }
+    }
+
     
     private func setupUI() {
         
