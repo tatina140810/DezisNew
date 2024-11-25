7137e5cc50b8b8c55a2a2742b9c579f0a5b64b8d
Nurtilek Abduraimov <abduraimovnurtilek4@gmail.com>
2024-11-25 20:17:26 +0600

    Изменил код

--- Dezis/Applications/SceneDelegate.swift
+++ Dezis/Applications/SceneDelegate.swift
@@ -2,123 +2,114 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
-    var window: UIWindow? 
+    var window: UIWindow?
     
-//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//    
-//        let socket = ChatWebSocket()
-//        socket.connect()
-//        socket.sendText(text: "Hello")
-//        socket.sendMessage(text: "Hello")
-//        //Проверяем валидность access token:
-//        print("isAccessTokenValid", isAccessTokenExpired())
-//        print(KeychainService.shared.accessToken, "access token")
-//        
-//     //   let tokenChat: UserTokenResponseModel
-//        //Test SignalR
-//        let signalR = SignalRService(url: URL(string: "wss://dezis.pp.ua/ws/chat/\(285)")!)
-//        signalR.connectionStart()
-//        
-//        //Test Chat post request
-//        
-//        ChatService.shared.sendMessage(message: ChatModel(text: "test")) { result in
-//            switch result {
-//            case .success(let success):
-//                ()
-//            case .failure(let failure):
-//                ()
-//            }
-//        }
-//        
-//        ChatService.shared.fetchChatList {  result in
-//            switch result {
-//            case .success(let success):
-//                ()
-//            case .failure(let failure):
-//                ()
-//            }
-//        }
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//        
-//        window = UIWindow(windowScene: windowScene)
-//        let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
-//        
-//        //TODO: - "Надо в зависимости от сохраненных токенов запускать либо логин, либо домашний экран"
-//        if hasCompletedOnboarding {
-//            if KeychainService.shared.hasToken {
-//                let clientTabBarVC = ClientTabBarController()
-//                window?.rootViewController = clientTabBarVC
-//            } else {
-//                let choiceVC = UINavigationController(rootViewController: ChoiceViewController())
-//                window?.rootViewController = choiceVC
-//            }
-//           
-//        } else {
-//            let root = UINavigationController(rootViewController: FirstOnboardingViewController())
-//            window?.rootViewController = root
-//        }
-//        
-//        window?.makeKeyAndVisible()
-//        if #available(iOS 15, *) {
-//            let navigationBarAppearance = UINavigationBarAppearance()
-//            navigationBarAppearance.configureWithOpaqueBackground()
-//            navigationBarAppearance.titleTextAttributes = [
-//                NSAttributedString.Key.foregroundColor: UIColor.white
-//            ]
-//            navigationBarAppearance.backgroundColor = UIColor(hex: "#1B2228")
-//            
-//            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
-//            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
-//            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
-//        } else {
-//            UINavigationBar.appearance().barTintColor = UIColor(hex: "#1B2228")
-//            UINavigationBar.appearance().titleTextAttributes = [
-//                NSAttributedString.Key.foregroundColor: UIColor.white
-//            ]
-//            UINavigationBar.appearance().isTranslucent = false
-//        }
-//    }
+    //    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    //
+    //        let socket = ChatWebSocket()
+    //        socket.connect()
+    //        socket.sendText(text: "Hello")
+    //        socket.sendMessage(text: "Hello")
+    //        //Проверяем валидность access token:
+    //        print("isAccessTokenValid", isAccessTokenExpired())
+    //        print(KeychainService.shared.accessToken, "access token")
+    //
+    //     //   let tokenChat: UserTokenResponseModel
+    //        //Test SignalR
+    //        let signalR = SignalRService(url: URL(string: "wss://dezis.pp.ua/ws/chat/\(285)")!)
+    //        signalR.connectionStart()
+    //
+    //        //Test Chat post request
+    //
+    //        ChatService.shared.sendMessage(message: ChatModel(text: "test")) { result in
+    //            switch result {
+    //            case .success(let success):
+    //                ()
+    //            case .failure(let failure):
+    //                ()
+    //            }
+    //        }
+    //
+    //        ChatService.shared.fetchChatList {  result in
+    //            switch result {
+    //            case .success(let success):
+    //                ()
+    //            case .failure(let failure):
+    //                ()
+    //            }
+    //        }
+    //        guard let windowScene = (scene as? UIWindowScene) else { return }
+    //
+    //        window = UIWindow(windowScene: windowScene)
+    //        let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
+    //
+    //        //TODO: - "Надо в зависимости от сохраненных токенов запускать либо логин, либо домашний экран"
+    //        if hasCompletedOnboarding {
+    //            if KeychainService.shared.hasToken {
+    //                let clientTabBarVC = ClientTabBarController()
+    //                window?.rootViewController = clientTabBarVC
+    //            } else {
+    //                let choiceVC = UINavigationController(rootViewController: ChoiceViewController())
+    //                window?.rootViewController = choiceVC
+    //            }
+    //
+    //        } else {
+    //            let root = UINavigationController(rootViewController: FirstOnboardingViewController())
+    //            window?.rootViewController = root
+    //        }
+    //
+    //        window?.makeKeyAndVisible()
+    //        if #available(iOS 15, *) {
+    //            let navigationBarAppearance = UINavigationBarAppearance()
+    //            navigationBarAppearance.configureWithOpaqueBackground()
+    //            navigationBarAppearance.titleTextAttributes = [
+    //                NSAttributedString.Key.foregroundColor: UIColor.white
+    //            ]
+    //            navigationBarAppearance.backgroundColor = UIColor(hex: "#1B2228")
+    //
+    //            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+    //            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+    //            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+    //        } else {
+    //            UINavigationBar.appearance().barTintColor = UIColor(hex: "#1B2228")
+    //            UINavigationBar.appearance().titleTextAttributes = [
+    //                NSAttributedString.Key.foregroundColor: UIColor.white
+    //            ]
+    //            UINavigationBar.appearance().isTranslucent = false
+    //        }
+    //    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-            
-            let roomId = 1
-            let token = KeychainService.shared.accessToken
-            
-            let chatWebSocket = ChatWebSocket(roomId: roomId, token: token)
-            chatWebSocket.connect()
-            chatWebSocket.sendText(text: "Hello")
-            
-            let signalRService = SignalRService(roomId: roomId, token: token)
-            signalRService.connectionStart()
-            
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            window = UIWindow(windowScene: windowScene)
-            
-            let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
-            
-            if hasCompletedOnboarding {
-                if KeychainService.shared.hasToken {
-                    let clientTabBarVC = ClientTabBarController()
-                    window?.rootViewController = clientTabBarVC
-                } else {
-                    let choiceVC = UINavigationController(rootViewController: ChoiceViewController())
-                    window?.rootViewController = choiceVC
-                }
+        
+        let roomId = 1
+        let token = KeychainService.shared.accessToken
+        
+        let chatWebSocket = ChatWebSocket(roomId: roomId, token: token)
+        chatWebSocket.connect()
+        chatWebSocket.sendText(text: "Hello")
+        
+        let signalRService = SignalRService(roomId: roomId, token: token)
+        signalRService.connectionStart()
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let hasCompletedOnboarding = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
+        
+        if hasCompletedOnboarding {
+            if KeychainService.shared.hasToken {
+                let clientTabBarVC = ClientTabBarController()
+                window?.rootViewController = clientTabBarVC
             } else {
-                let root = UINavigationController(rootViewController: FirstOnboardingViewController())
-                window?.rootViewController = root
+                let choiceVC = UINavigationController(rootViewController: ChoiceViewController())
+                window?.rootViewController = choiceVC
             }
-            
-            window?.makeKeyAndVisible()
+        } else {
+            let root = UINavigationController(rootViewController: FirstOnboardingViewController())
+            window?.rootViewController = root
         }
         
-        NotificationCenter.default.addObserver(self, selector: #selector(goToMain), name: NSNotification.Name("GoToMain"), object: nil)
-        
         window?.makeKeyAndVisible()
-    }
-    
-    @objc private func goToMain() {
-        window?.rootViewController = ClientTabBarController()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -141,7 +132,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
     }
     
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
 }
